c7b8e3643e694941b13a4a52bd8f713c07349c2e
Swati Maurya <swatimaurya704@gmail.com>
2021-05-19 10:15:35 +0530

    initial commit

---
+++ src/test/java/com/softhinkers/stepdefinition/Login.java
@@ -0,0 +1,29 @@
+package com.softhinkers.stepdefinition;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+
+public class Login {
+    @Given(": User is on NetBanking landing page")
+    public void userIsOnNetBankingLandingPage() {
+
+    }
+
+    @When(": User login into application with username and password")
+    public void userLoginIntoApplicationWithUsernameAndPassword() {
+
+    }
+
+    @Then(": Home page is populated")
+    public void homePageIsPopulated() {
+
+    }
+
+    @And(": Cards are displayed")
+    public void cardsAreDisplayed() {
+    }
+}
---
+++ src/test/java/com/softhinkers/testrunner/TestRunner.java
@@ -0,0 +1,10 @@
+package com.softhinkers.testrunner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith( Cucumber.class )
+@CucumberOptions( glue = "com.softhinkers.stepdefinition"  )
+public class TestRunner {
+}
